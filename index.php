8834b91d21b52c944fd5f3b65319ca2eff492dea
M. D. Ahwireng <kaaymike@hotmail.co.uk>
2023-06-06 09:04:49 +0000

    Adds landing page for the project

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php require_once 'assets/php/head.php'; ?>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>BarbSal Landing</title>
+</head>
+<body>
+    <div id='landing'>
+        <div class="container">
+            <div class="row">
+                <div class="welcome-box">
+                    <div class="welcome-box-content">
+                        <h1>Welcome to BarbSal</h1>
+                        <p>
+                        Control your salon and barbershop appointments
+                        </p>
+                    </div>
+                <div class="welcome-button">
+                    <button class="btn btn-lg btn-danger rounded-pill"><h1>Hop In</h1></button> 
+                </div>   
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
